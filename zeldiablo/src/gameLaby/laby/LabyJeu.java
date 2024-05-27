325a6964bcb317ad97bf8231d5e9bdf65ba6fea7
Rémi Choffat <remi.choffat@gmail.com>
2024-05-27 15:24:01 +0200

    Modification du diagramme de seq et correction des commentaires

--- zeldiablo/src/gameLaby/laby/LabyJeu.java
+++ zeldiablo/src/gameLaby/laby/LabyJeu.java
@@ -1,44 +1,43 @@
 package gameLaby.laby;
 
-
 import moteurJeu.Clavier;
 
 import java.io.IOException;
 
 public class LabyJeu implements moteurJeu.Jeu {
 
-    private final Labyrinthe laby;
+   private final Labyrinthe laby;
 
-    public LabyJeu() throws IOException {
-        laby = new Labyrinthe("labySimple/laby1.txt");
-    }
+   public LabyJeu() throws IOException {
+      laby = new Labyrinthe("labySimple/laby1.txt");
+   }
 
-    @Override
-    public void update(double secondes, Clavier clavier) {
-        if (clavier.droite) {
-            laby.deplacerPerso(Labyrinthe.DROITE);
-        }
+   @Override
+   public void update(double secondes, Clavier clavier) {
+      if (clavier.droite) {
+         laby.deplacerPerso(Labyrinthe.DROITE);
+      }
 
-        if (clavier.gauche) {
-            laby.deplacerPerso(Labyrinthe.GAUCHE);
-        }
+      if (clavier.gauche) {
+         laby.deplacerPerso(Labyrinthe.GAUCHE);
+      }
 
-        if (clavier.haut) {
-            laby.deplacerPerso(Labyrinthe.HAUT);
-        }
+      if (clavier.haut) {
+         laby.deplacerPerso(Labyrinthe.HAUT);
+      }
 
-        if (clavier.bas) {
-            laby.deplacerPerso(Labyrinthe.BAS);
-        }
-    }
+      if (clavier.bas) {
+         laby.deplacerPerso(Labyrinthe.BAS);
+      }
+   }
 
-    @Override
-    public void init() {
+   @Override
+   public void init() {
 
-    }
+   }
 
-    @Override
-    public boolean etreFini() {
-        return false;
-    }
+   @Override
+   public boolean etreFini() {
+      return false;
+   }
 }
--- zeldiablo/src/gameLaby/laby/Labyrinthe.java
+++ zeldiablo/src/gameLaby/laby/Labyrinthe.java
@@ -11,188 +11,188 @@ import java.io.IOException;
  */
 public class Labyrinthe {
 
-    /**
-     * Constantes char
-     */
-    public static final char MUR = 'X';
-    public static final char PJ = 'P';
-    public static final char VIDE = '.';
+   /**
+    * Constantes char
+    */
+   public static final char MUR = 'X';
+   public static final char PJ = 'P';
+   public static final char VIDE = '.';
 
-    /**
-     * constantes actions possibles
-     */
-    public static final String HAUT = "Haut";
-    public static final String BAS = "Bas";
-    public static final String GAUCHE = "Gauche";
-    public static final String DROITE = "Droite";
+   /**
+    * constantes actions possibles
+    */
+   public static final String HAUT = "Haut";
+   public static final String BAS = "Bas";
+   public static final String GAUCHE = "Gauche";
+   public static final String DROITE = "Droite";
 
-    /**
-     * attribut du personnage
-     */
-    public Perso pj;
+   /**
+    * attribut du personnage
+    */
+   public Perso pj;
 
-    /**
-     * les murs du labyrinthe
-     */
-    public boolean[][] murs;
+   /**
+    * les murs du labyrinthe
+    */
+   public boolean[][] murs;
 
-    /**
-     * retourne la case suivante selon une actions
-     *
-     * @param x      case depart
-     * @param y      case depart
-     * @param action action effectuee
-     * @return case suivante
-     */
-    static int[] getSuivant(int x, int y, String action) {
-        switch (action) {
-            case HAUT:
-                // on monte une ligne
-                y--;
-                break;
-            case BAS:
-                // on descend une ligne
-                y++;
-                break;
-            case DROITE:
-                // on augmente colonne
-                x++;
-                break;
-            case GAUCHE:
-                // on augmente colonne
-                x--;
-                break;
-            default:
-                throw new Error("action inconnue");
-        }
-        int[] res = {x, y};
-        return res;
-    }
+   /**
+    * retourne la case suivante selon une actions
+    *
+    * @param x      case depart
+    * @param y      case depart
+    * @param action action effectuee
+    * @return case suivante
+    */
+   static int[] getSuivant(int x, int y, String action) {
+      switch (action) {
+         case HAUT:
+            // on monte une ligne
+            y--;
+            break;
+         case BAS:
+            // on descend une ligne
+            y++;
+            break;
+         case DROITE:
+            // on augmente colonne
+            x++;
+            break;
+         case GAUCHE:
+            // on diminue colonne
+            x--;
+            break;
+         default:
+            throw new Error("action inconnue");
+      }
+      return new int[]{x, y};
+   }
 
-    /**
-     * charge le labyrinthe
-     *
-     * @param nom nom du fichier de labyrinthe
-     * @return labyrinthe cree
-     * @throws IOException probleme a la lecture / ouverture
-     */
-    public Labyrinthe(String nom) throws IOException {
-        // ouvrir fichier
-        FileReader fichier = new FileReader(nom);
-        BufferedReader bfRead = new BufferedReader(fichier);
+   /**
+    * charge le labyrinthe
+    *
+    * @param nom nom du fichier de labyrinthe
+    * @return labyrinthe cree
+    * @throws IOException probleme a la lecture / ouverture
+    */
+   public Labyrinthe(String nom) throws IOException {
+      // ouvrir fichier
+      FileReader fichier = new FileReader(nom);
+      BufferedReader bfRead = new BufferedReader(fichier);
 
-        int nbLignes, nbColonnes;
-        // lecture nblignes
-        nbLignes = Integer.parseInt(bfRead.readLine());
-        // lecture nbcolonnes
-        nbColonnes = Integer.parseInt(bfRead.readLine());
+      int nbLignes, nbColonnes;
+      // lecture nblignes
+      nbLignes = Integer.parseInt(bfRead.readLine());
+      // lecture nbcolonnes
+      nbColonnes = Integer.parseInt(bfRead.readLine());
 
-        // creation labyrinthe vide
-        this.murs = new boolean[nbColonnes][nbLignes];
-        this.pj = null;
+      // creation labyrinthe vide
+      this.murs = new boolean[nbColonnes][nbLignes];
+      this.pj = null;
 
-        // lecture des cases
-        String ligne = bfRead.readLine();
+      // lecture des cases
+      String ligne = bfRead.readLine();
 
-        // stocke les indices courants
-        int numeroLigne = 0;
+      // stocke les indices courants
+      int numeroLigne = 0;
 
-        // parcours le fichier
-        while (ligne != null) {
+      // parcours le fichier
+      while (ligne != null) {
 
-            // parcours de la ligne
-            for (int colonne = 0; colonne < ligne.length(); colonne++) {
-                char c = ligne.charAt(colonne);
-                switch (c) {
-                    case MUR:
-                        this.murs[colonne][numeroLigne] = true;
-                        break;
-                    case VIDE:
-                        this.murs[colonne][numeroLigne] = false;
-                        break;
-                    case PJ:
-                        // pas de mur
-                        this.murs[colonne][numeroLigne] = false;
-                        // ajoute PJ
-                        this.pj = new Perso(colonne, numeroLigne);
-                        break;
+         // parcours de la ligne
+         for (int colonne = 0; colonne < ligne.length(); colonne++) {
+            char c = ligne.charAt(colonne);
+            switch (c) {
+               case MUR:
+                  this.murs[colonne][numeroLigne] = true;
+                  break;
+               case VIDE:
+                  this.murs[colonne][numeroLigne] = false;
+                  break;
+               case PJ:
+                  // pas de mur
+                  this.murs[colonne][numeroLigne] = false;
+                  // ajoute PJ
+                  this.pj = new Perso(colonne, numeroLigne);
+                  break;
 
-                    default:
-                        throw new Error("caractere inconnu " + c);
-                }
+               default:
+                  throw new Error("caractere inconnu " + c);
             }
+         }
 
-            // lecture
-            ligne = bfRead.readLine();
-            numeroLigne++;
-        }
+         // lecture
+         ligne = bfRead.readLine();
+         numeroLigne++;
+      }
 
-        // ferme fichier
-        bfRead.close();
-    }
-
-
-    /**
-     * deplace le personnage en fonction de l'action.
-     * gere la collision avec les murs
-     *
-     * @param action une des actions possibles
-     */
-    public void deplacerPerso(String action) {
-        // case courante
-        int[] courante = {this.pj.x, this.pj.y};
-
-        // calcule case suivante
-        int[] suivante = getSuivant(courante[0], courante[1], action);
-
-        // si c'est pas un mur, on effectue le deplacement
-        if (!this.murs[suivante[0]][suivante[1]]) {
-            // on met a jour personnage
-            this.pj.x = suivante[0];
-            this.pj.y = suivante[1];
-        }
-    }
+      // ferme fichier
+      bfRead.close();
+   }
 
 
-    /**
-     * jamais fini
-     *
-     * @return fin du jeu
-     */
-    public boolean etreFini() {
-        return false;
-    }
+   /**
+    * deplace le personnage en fonction de l'action.
+    * gere la collision avec les murs
+    *
+    * @param action une des actions possibles
+    */
+   public void deplacerPerso(String action) {
+      // case courante
+      int[] courante = {this.pj.x, this.pj.y};
 
-    // ##################################
-    // GETTER
-    // ##################################
+      // calcule case suivante
+      int[] suivante = getSuivant(courante[0], courante[1], action);
 
-    /**
-     * return taille selon Y
-     *
-     * @return
-     */
-    public int getLengthY() {
-        return murs[0].length;
-    }
+      // si ce n'est pas un mur, on effectue le deplacement
+      if (!this.murs[suivante[0]][suivante[1]]) {
+         // on met a jour personnage
+         this.pj.x = suivante[0];
+         this.pj.y = suivante[1];
+      }
+   }
 
-    /**
-     * return taille selon X
-     *
-     * @return
-     */
-    public int getLength() {
-        return murs.length;
-    }
 
-    /**
-     * return mur en (i,j)
-     * @param x
-     * @param y
-     * @return
-     */
-    public boolean getMur(int x, int y) {
-        // utilise le tableau de boolean
-        return this.murs[x][y];
-    }
+   /**
+    * jamais fini
+    *
+    * @return fin du jeu
+    */
+   public boolean etreFini() {
+      return false;
+   }
+
+   // ##################################
+   // GETTER
+   // ##################################
+
+   /**
+    * return taille selon Y
+    *
+    * @return taille selon Y
+    */
+   public int getLengthY() {
+      return murs[0].length;
+   }
+
+   /**
+    * return taille selon X
+    *
+    * @return taille selon X
+    */
+   public int getLength() {
+      return murs.length;
+   }
+
+   /**
+    * return mur en (i,j)
+    *
+    * @param x coordonnee x
+    * @param y coordonnee y
+    * @return mur en (i,j)
+    */
+   public boolean getMur(int x, int y) {
+      // utilise le tableau de boolean
+      return this.murs[x][y];
+   }
 }

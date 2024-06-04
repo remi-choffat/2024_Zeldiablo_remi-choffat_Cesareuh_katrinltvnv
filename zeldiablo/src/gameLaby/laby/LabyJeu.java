ddb33a67248680655b36decdb1291f853eddf872
Rémi Choffat <remi.choffat@gmail.com>
2024-06-04 11:44:00 +0200

    Retour aux modifications de déplacement

--- zeldiablo/src/gameLaby/laby/LabyJeu.java
+++ zeldiablo/src/gameLaby/laby/LabyJeu.java
@@ -6,28 +6,31 @@ import java.io.IOException;
 
 public class LabyJeu implements moteurJeu.Jeu {
 
-    public final Labyrinthe laby;
+   public final Labyrinthe laby;
+   boolean deplacement = false;
 
    public LabyJeu() throws IOException {
-      laby = new Labyrinthe("labySimple/laby1.txt");
+      laby = new Labyrinthe("labySimple/labyMonstre.txt");
    }
 
    @Override
    public void update(double secondes, Clavier clavier) {
-      if (clavier.droite) {
-         laby.deplacerPerso(Labyrinthe.DROITE);
-      }
+      if (!deplacement) {
+         if (clavier.droite) {
+            laby.deplacerPerso(Labyrinthe.DROITE);
+         }
 
-      if (clavier.gauche) {
-         laby.deplacerPerso(Labyrinthe.GAUCHE);
-      }
+         if (clavier.gauche) {
+            laby.deplacerPerso(Labyrinthe.GAUCHE);
+         }
 
-      if (clavier.haut) {
-         laby.deplacerPerso(Labyrinthe.HAUT);
-      }
+         if (clavier.haut) {
+            laby.deplacerPerso(Labyrinthe.HAUT);
+         }
 
-      if (clavier.bas) {
-         laby.deplacerPerso(Labyrinthe.BAS);
+         if (clavier.bas) {
+            laby.deplacerPerso(Labyrinthe.BAS);
+         }
       }
    }
 
